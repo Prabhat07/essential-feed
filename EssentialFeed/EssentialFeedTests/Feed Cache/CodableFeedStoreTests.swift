d6ee5a827e7ee76d092146b0288aa06dd8ef265b
Prabhat <prabhat.tiwari43@gmail.com>
2025-05-31 08:27:06 +0530

    Extract system uder test (sut) creation into a factory method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -75,7 +75,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSut()
         
         let exp = expectation(description: "Wait for completion")
         sut.retrieve { result in
@@ -92,7 +92,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSut()
         
         let exp = expectation(description: "Wait for completion")
         sut.retrieve { firstResult in
@@ -112,7 +112,7 @@ final class CodableFeedStoreTests: XCTestCase {
 
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSut()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         
@@ -138,4 +138,10 @@ final class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
+    //MARK: - Helpers
+    
+    func makeSut() -> CodableFeedStore {
+        return CodableFeedStore()
+    }
+    
 }
