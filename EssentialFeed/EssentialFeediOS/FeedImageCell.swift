d4ae79f3693af27b9d98f3fc670755bfe4a4af1e
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-03 12:42:50 +0530

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Prabhat Tiwari on 03/07/25.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
