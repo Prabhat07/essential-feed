6118c82bc5932810b96d1d26fc9d24f8a1fb8892
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-03 12:42:50 +0530

    Render loaded feed on successful load completion

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -12,6 +12,7 @@ final public class FeedViewController: UITableViewController {
     
     private var loader: FeedLoader?
     private var viewIsApperaing: ((FeedViewController) -> ())?
+    private var tableModel = [FeedImage]()
     
     public convenience init(loader: FeedLoader) {
         self.init()
@@ -34,9 +35,24 @@ final public class FeedViewController: UITableViewController {
     
     @objc func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
             self?.refreshControl?.endRefreshing()
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
         }
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
     
 }
