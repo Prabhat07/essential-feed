2a25361a2e9f1c235bd0c5c00a4d3c95bda2db50
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-03 19:22:02 +0530

    Cancel image loading when image view is not visible anymore

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -92,6 +92,24 @@ final class FeedViewControllerTests: XCTestCase {
         
     }
     
+    func test_feedImageView_cancelsImageLoadingWhenNotVisibleAnymore() {
+        let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateViewAppearance()
+        
+        loader.completeFeedLoading(with: [image0, image1], at: 0)
+        
+        XCTAssertEqual(loader.cancelledImageURLs, [], "Expected no image URL requests until views become visible")
+        
+        sut.simulateFeedImageViewNotVisible(at: 0)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.url], "Expected first image URL request once first view becomes visible")
+        
+        sut.simulateFeedImageViewNotVisible(at: 1)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second image URL request once second view also becomes visible")
+    }
+    
     // MARK: - Helpers
     
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedViewController, LoaderSpy) {
@@ -141,6 +159,7 @@ final class FeedViewControllerTests: XCTestCase {
         }
         
         private(set) var loadedImageURLs: [URL] = []
+        private(set) var cancelledImageURLs = [URL]()
         
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             feedRequests.append(completion)
@@ -159,6 +178,10 @@ final class FeedViewControllerTests: XCTestCase {
         
         func loadImageData(from url: URL) {
             loadedImageURLs.append(url)
+        }
+        
+        func cancelImageDataLoad(from url: URL) {
+            cancelledImageURLs.append(url)
         }
     }
 }
@@ -203,8 +226,17 @@ private extension FeedViewController {
         return refreshControl?.isRefreshing == true
     }
     
-    func simulateFeedImageViewVisible(at index: Int) {
-        _ = feedImageView(at: index)
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+         return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    func simulateFeedImageViewNotVisible(at row: Int) {
+        let view = simulateFeedImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
     }
     
     func numberOfRenderedFeedImageViews() -> Int {
