2e30a3f33cf58938e2f8c2bc85574e2745ae3429
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-07 22:43:37 +0530

    Move `FeedRefreshController` to seperatre file

--- EssentialFeed/EssentialFeediOS/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Controllers/FeedViewController.swift
@@ -8,32 +8,6 @@
 import UIKit
 import EssentialFeed
 
-final public class FeedRefreshController: NSObject {
-    public lazy var view: UIRefreshControl = {
-        let view = UIRefreshControl()
-        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        return view
-    }()
-    
-    private let feedLoader: FeedLoader
-    
-    var onRefresh: (([FeedImage]) -> Void)?
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    @objc func refresh() {
-        view.beginRefreshing()
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.onRefresh?(feed)
-            }
-            self?.view.endRefreshing()
-        }
-    }
-}
-
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     
     public var refreshController: FeedRefreshController?
