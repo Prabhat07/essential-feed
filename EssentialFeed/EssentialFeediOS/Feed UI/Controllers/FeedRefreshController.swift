66c8462dc97cd042ecda988a0847b41dff1321bc
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-08 14:18:42 +0530

    Group files in a new `Feed UI` folder

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedRefreshController.swift
+//  EssentialFeediOS
+//
+//  Created by Prabhat Tiwari on 07/07/25.
+//
+
+import UIKit
+import EssentialFeed
+
+final public class FeedRefreshController: NSObject {
+    public lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private let feedLoader: FeedLoader
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
