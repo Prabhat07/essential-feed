e50f4c2dd7922a0451b5c0b22bf0d474106a017b
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-16 14:55:34 +0530

    Move `FeedLoaderPresentationAdapter` to separate file

--- EssentialFeed/EssentialFeediOS/Composer/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Composer/FeedUIComposer.swift
@@ -58,25 +58,3 @@ final class FeedViewAdapter: FeedView {
     }
 
 }
-
-final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    let feedLoader: FeedLoader
-    var presenter: FeedPresenter?
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    func didRequestFeedRefresh() {
-        presenter?.didStartLoadingFeed()
-        feedLoader.load { [weak self] result in
-            switch result {
-            case .success(let feed):
-                self?.presenter?.didFinishLoadingFeed(with: feed)
-            case .failure(let error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-    
-}
