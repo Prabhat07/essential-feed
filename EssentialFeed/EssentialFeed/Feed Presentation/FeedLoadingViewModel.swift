8dbb3ac94fafccbe03aa06b5e567cc1f1654cc02
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-18 17:08:22 +0530

    Move `FeedViewModel`, `FeedLoadingViewModel` and `FeedErrorViewModel` to separate file Move  to separate file.

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Prabhat Tiwari on 18/07/25.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Prabhat Tiwari on 18/07/25.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
