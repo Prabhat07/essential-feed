ecff9fd635f392c60b5ac3464a72f635ea9f2871
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-18 10:28:22 +0530

    Make messages Array a Set to make test more permissive since we don't care about order. (Changing order should not break the tests!)

--- EssentialFeed/EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presenter/FeedPresenterTests.swift
@@ -37,7 +37,7 @@ class FeedPresenter {
     }
     
     func didStartLoadingFeed() {
-        errorView.display(FeedErrorViewModel(message: nil))
+        errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 }
@@ -70,19 +70,19 @@ final class FeedPresenterTests: XCTestCase {
     
     class ViewSpy: FeedLoadingView, FeedErrorView {
         
-        enum Message: Equatable {
+        enum Message: Hashable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
         }
         
-        var messages = [Message]()
+        var messages = Set<Message>()
         
         func display(_ viewModel: FeedErrorViewModel) {
-            messages.append(.display(errorMessage: viewModel.message))
+            messages.insert(.display(errorMessage: viewModel.message))
         }
         
         func display(_ viewModel: FeedLoadingViewModel) {
-            messages.append(.display(isLoading: viewModel.isLoading))
+            messages.insert(.display(isLoading: viewModel.isLoading))
         }
         
     }
