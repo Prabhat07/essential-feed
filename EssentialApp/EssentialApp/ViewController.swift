505bd678044e2087ccf0c6f31b66ef0ccde12a72
Prabhat <prabhat.tiwari43@gmail.com>
2025-07-25 20:49:20 +0530

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Prabhat Tiwari on 25/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Prabhat Tiwari on 25/07/25.
+//
+
+import Testing
+
+struct EssentialAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
