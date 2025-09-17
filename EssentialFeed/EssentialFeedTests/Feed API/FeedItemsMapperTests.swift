81c73858d1b7978817bfab8e1f6b259114d6b07a
Prabhat <prabhat.tiwari43@gmail.com>
2025-09-17 17:42:06 +0530

    Test FeedItemsMapper in isolation

---
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -0,0 +1,87 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Prabhat Tiwari on 01/05/25.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedItemsMapperTests: XCTestCase {
+
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let data = makeItemJSON([])
+        
+        let samples = [199, 201, 300, 400, 500]
+            
+        try samples.forEach { code in
+            XCTAssertThrowsError (
+                try FeedItemsMapper.map(data, from: HTTPURLResponse(statusCode: code))
+            )
+        }
+        
+    }
+    
+    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
+        let invalidJson = Data("Invlaid JSON".utf8)
+        
+        XCTAssertThrowsError (
+            try FeedItemsMapper.map(invalidJson, from: HTTPURLResponse(statusCode: 200))
+        )
+        
+    }
+    
+    func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
+        let emptyListJSON = makeItemJSON([])
+        
+        let result = try FeedItemsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(
+            id: UUID(),
+            imageURL: URL(string: "http://a-url.com")!)
+        
+        let item2 = makeItem(
+            id: UUID(),
+            description: "a description",
+            location: "a location",
+            imageURL: URL(string: "http://another-url.com")!)
+        
+        let json = makeItemJSON([item1.json, item2.json])
+        
+        let result = try FeedItemsMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [item1.model, item2.model])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
+        let item = FeedImage(id: id,
+                            description: description,
+                            location: location,
+                            url: imageURL)
+        let json = ["id": id.uuidString,
+                    "description": description,
+                    "location": location,
+                    "image": imageURL.absoluteString]
+            .compactMapValues { $0 }
+        return (item, json)
+    }
+    
+    private func makeItemJSON(_ items:[[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
+}
+
+private extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
