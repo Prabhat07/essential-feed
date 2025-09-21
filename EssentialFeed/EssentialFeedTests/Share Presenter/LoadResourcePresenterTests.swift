f3d90fe7776729b81b109eee1a04a0f38903a465
Prabhat <prabhat.tiwari43@gmail.com>
2025-09-21 10:08:06 +0530

    Rename method

--- EssentialFeed/EssentialFeedTests/Share Presenter/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Share Presenter/LoadResourcePresenterTests.swift
@@ -16,10 +16,10 @@ final class LoadResourcePresenterTests: XCTestCase {
         XCTAssert(view.messages.isEmpty)
     }
     
-    func test_didStartLoadingFeed_displaysNoErrorMessageAndStartsLoading() {
+    func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
         
-        sut.didStartLoadingFeed()
+        sut.didStartLoading()
         
         XCTAssertEqual(view.messages, [.display(errorMessage: .none), .display(isLoading: true)])
     }
