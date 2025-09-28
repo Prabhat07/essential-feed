4478d4130384ecc59cd8d7b0ee433594f2e3e4ad
Prabhat <prabhat.tiwari43@gmail.com>
2025-09-28 18:57:21 +0530

    Render comments

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -28,7 +28,7 @@ extension ListViewController {
                 fake.addTarget(target, action: Selector($0), for: .valueChanged)
             }
         }
-    
+        
         refreshControl = fake
     }
     
@@ -47,7 +47,40 @@ extension ListViewController {
     func simulateErrorViewTap() {
         errorView.simulateTap()
     }
+}
+
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 :  tableView.numberOfRows(inSection: commentsSection)
+    }
+
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+
+    private var commentsSection: Int {
+        return 0
+    }
+}
     
+extension ListViewController {
     @discardableResult
     func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
          return feedImageView(at: index) as? FeedImageCell
