7d6c190ba93492108fca25b510fb3ed1a41356d4
Prabhat <prabhat.tiwari43@gmail.com>
2025-10-02 12:48:42 +0530

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -11,7 +11,8 @@ import Combine
 
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     let loader: () -> AnyPublisher<Resource, Error>
-    var presenter: LoadResourcePresenter<Resource, View>?
+    var presenter: LoadResourcePresenter<Resource, View>? 
+    private var isLoading: Bool = false
     private var cancellable: Cancellable?
     
     init(loader:@escaping () -> AnyPublisher<Resource, Error>) {
@@ -19,10 +20,16 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
+        guard !isLoading else { return }
+        
         presenter?.didStartLoading()
+        isLoading = true
         
         cancellable = loader()
             .dispatchOnMainQueue()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
             .sink(
             receiveCompletion: { [weak self] completion in
                 switch completion {
@@ -31,10 +38,10 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
                 case let .failure(error):
                     self?.presenter?.didFinishLoading(with: error)
                 }
+                self?.isLoading = false
             }, receiveValue: { [weak self] feed in
                 self?.presenter?.didFinishLoading(with: feed)
             })
-        
     }
     
 }
--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -27,11 +27,14 @@ class CommentsUIIntegrationTests: XCTestCase {
         XCTAssertEqual(loader.loadCommentsCallCount, 0, "Expected no loading requests before view is loaded")
         
         sut.simulateViewAppearance()
-        XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected a loading request once view is loaded")
+        sut.simulateUserInitiatedReload()
+        XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected no request until previous completes")
         
+        loader.completeCommentsLoading(at: 0)
         sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadCommentsCallCount, 2, "Expected another loading request once user initiates a reload")
         
+        loader.completeCommentsLoading(at: 1)
         sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadCommentsCallCount, 3, "Expected yet another loading request once user initiates another reload")
     }
@@ -196,6 +199,7 @@ class CommentsUIIntegrationTests: XCTestCase {
         
         func completeCommentsLoading(with feed: [ImageComment] = [], at index: Int = 0) {
             requests[index].send(feed)
+            requests[index].send(completion: .finished)
         }
         
         func completeCommentsLoadingWithError(at index: Int = 0) {
